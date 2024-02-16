84e38b83c43ebb24739d639cc349c3d7000a899b
Yanzhou99 <89799098+Yanzhou99@users.noreply.github.com>
2024-02-15 21:12:12 -0600

    add mob and health bar

---
+++ Assets/Code/Mob.cs
@@ -0,0 +1,49 @@
+//reference: https://www.youtube.com/watch?v=_lREXfAMUcE
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mob : MonoBehaviour
+{
+
+    public static event Action<Mob> OnEnemykilled;
+    [SerializeField] float health, maxHealth = 3f;
+
+    [SerializeField] HealthBar healthBar;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<HealthBar>();
+    }
+
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        //target = Gameobject.Find("Player");
+    }
+
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+  
+}
+
+
+
+  
