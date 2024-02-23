a835a0a73e3aac45cdc21141098205483601e91c
Yanzhou99 <89799098+Yanzhou99@users.noreply.github.com>
2024-02-23 17:16:41 -0600

    Add finish line, and fix some background bug
    I add the finish line and the corresponding code, which make the player jump to next level when touching it. I also fixed some background bug, such as moving rocks.

---
+++ Assets/Code/FinishLine.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+
+
+{
+    // If player touch the finish line, it will jump to the next scene in the building(next level).
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
