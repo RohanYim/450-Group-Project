1e1cf37dc6c76ab3351d24cd516d06dc29c89c46
Lunchi <g.lunchi@wustl.edu>
2024-04-10 23:05:17 -0500

    add audio volume control and make bigMob's fire can track player

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioSource audioSource; 
+    public float volumeChangeAmount = 0.1f; 
     public void PlayGame()
     {
         GameManager.Instance.ResetTotalTime();
@@ -14,6 +17,30 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+
+
+    public void IncreaseVolume()
+    {
+        if (audioSource.volume < 1)
+        {
+            audioSource.volume += volumeChangeAmount;
+            
+        }
+    }
+
+    public void DecreaseVolume()
+    {
+        if (audioSource.volume > 0)
+        {
+            audioSource.volume -= volumeChangeAmount;
+            if (audioSource.volume < 0)
+            {
+                print(1);
+            }
+ 
+        }
     }
 
 }
